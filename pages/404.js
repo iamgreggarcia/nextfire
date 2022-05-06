c62fd32ba3214215a55ffd50343ef686fd93083a
iamgreggarcia <16596972+iamgreggarcia@users.noreply.github.com>
2022-05-06 07:22:39 -0400

    Change img to Image component on 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,14 @@
+import Image from "next/image";
 import Link from "next/link";
+import homerGif from "../public/homer.gif";
 
 export default function Custom404() {
   return (
     <main>
       <h1>404</h1>
       <h2>That page does not seem to exist...</h2>
-      <div className="box-center">
-        <img src={"/homer.gif"} className="card-img" />
+      <div className="card-img">
+        <Image src={homerGif} alt="Homer Simpson frantically searching" />
       </div>
       <Link href="/">
         <button className="btn-blue">Go home</button>
