b268b0f536d840cdecc524b669f326710094b77c
iamgreggarcia <16596972+iamgreggarcia@users.noreply.github.com>
2022-05-06 09:11:00 -0400

    Add post editing form

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -1,11 +1,114 @@
 import MetaTags from "../../components/Metatags";
+import { firestore, auth, serverTimestamp } from "../../lib/firebase";
 import AuthCheck from "../../components/AuthCheck";
+import styles from "../../styles/Admin.module.css";
+
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import { useForm } from "react-hook-form";
+import ReactMarkdown from "react-markdown";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import toast, { useToasterStore } from "react-hot-toast";
 
 export default function AdminPostsEdit({}) {
   return (
-    <main>
-      <MetaTags title="admin page" />
-      <h1>Edit Posts</h1>
+    <AuthCheck>
+      <PostManager />
+    </AuthCheck>
+  );
+}
+
+function PostManager() {
+  const [preview, setPreview] = useState(false);
+
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const postRef = firestore
+    .collection("users")
+    .doc(auth.currentUser.uid)
+    .collection("posts")
+    .doc(slug);
+  // see also: useDocumentDataOnce()
+  const [post] = useDocumentData(postRef);
+
+  return (
+    <main className={styles.container}>
+      {post && (
+        <>
+          <section>
+            <h1>{post.title}</h1>
+            <p>ID: {post.slug}</p>
+
+            <PostForm
+              postRef={postRef}
+              defaultValues={post}
+              preview={preview}
+            />
+          </section>
+          <aside>
+            <h3>Tools</h3>
+            <button onClick={() => setPreview(!preview)}>
+              {preview ? "Edit" : "Preview"}
+            </button>
+            <Link href={`/${post.username}/${post.slug}`}>
+              <button className="btn-blue">Live view</button>
+            </Link>
+          </aside>
+        </>
+      )}
     </main>
+  );
+}
+
+function PostForm({ defaultValues, postRef, preview }) {
+  const { register, handleSubmit, reset, watch } = useForm({
+    defaultValues,
+    mode: "onChange",
+  });
+
+  const updatePost = async ({ content, published }) => {
+    await postRef.update({
+      content,
+      published,
+      updatedAt: serverTimestamp(),
+    });
+
+    // need to reset the validation state of the form
+    reset({ content, published });
+
+    toast.success("Posts updated successfully!");
+  };
+
+  return (
+    <form onSubmit={handleSubmit(updatePost)}>
+      {preview && (
+        <div className="card">
+          <ReactMarkdown>{watch("content")}</ReactMarkdown>
+        </div>
+      )}
+      <div className={preview ? styles.hidden : styles.controls}>
+        {/* <textarea name="content" ref={register}></textarea> */}
+        <textarea {...register("content")} />
+        <fieldset>
+          {/* <input
+            className={styles.checkbox}
+            name="published"
+            type="checkbox"
+            ref={register}
+          /> */}
+          <input
+            type="checkbox"
+            className={styles.checkbox}
+            {...register("published")}
+          />
+          <label>Published</label>
+        </fieldset>
+        <button type="submit" className="btn-green">
+          Save Changes
+        </button>
+      </div>
+    </form>
   );
 }
--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,11 +1,101 @@
 import AuthCheck from "../../components/AuthCheck";
+import { firestore, auth } from "../../lib/firebase";
+import PostFeed from "../../components/PostFeed";
+import { UserContext } from "../../lib/context";
+import { useContext, useState } from "react";
+import { useRouter } from "next/router";
+
+import { useCollection } from "react-firebase-hooks/firestore";
+import { kebabCase } from "lodash";
+import toast from "react-hot-toast";
+import { async } from "@firebase/util";
+import { serverTimestamp } from "firebase/firestore";
+import styles from "../../styles/Admin.module.css";
 
 export default function AdminPostsPage({}) {
   return (
     <main>
       <AuthCheck>
-        <h1>Admin Posts</h1>
+        <PostList />
+        <CreateNewPost />
       </AuthCheck>
     </main>
+  );
+}
+
+function PostList() {
+  const ref = firestore
+    .collection("users")
+    .doc(auth.currentUser.uid)
+    .collection("posts");
+
+  const query = ref.orderBy("createdAt");
+  const [querySnapshot] = useCollection(query);
+
+  const posts = querySnapshot?.docs.map((doc) => doc.data());
+
+  return (
+    <>
+      <h1>Manage your posts</h1>
+      <PostFeed posts={posts} admin />
+    </>
+  );
+}
+
+function CreateNewPost() {
+  const router = useRouter();
+  const { username } = useContext(UserContext);
+  const [title, setTitle] = useState("");
+
+  // Verify slug is URL safe
+  const slug = encodeURI(kebabCase(title));
+
+  // Validate title length
+  const isValid = title.length > 3 && title.length < 100;
+
+  const createPost = async (e) => {
+    e.preventDefault();
+    const uid = auth.currentUser.uid;
+    const ref = firestore
+      .collection("users")
+      .doc(uid)
+      .collection("posts")
+      .doc(slug);
+
+    // Give all fields a default value here
+    const data = {
+      title,
+      slug,
+      uid,
+      username,
+      published: false,
+      content: "# hello, world.",
+      createAt: serverTimestamp(),
+      updatedAt: serverTimestamp(),
+      heartCount: 0,
+    };
+
+    await ref.set(data);
+    toast.success("Post created!");
+
+    // Imperative navigation after document is set
+    router.push(`/admin/${slug}`);
+  };
+
+  return (
+    <form onSubmit={createPost}>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="My awesome article title"
+        className={styles.input}
+      />
+      <p>
+        <strong>Slug:</strong> {slug}
+      </p>
+      <button type="submit" disabled={!isValid} className="btn-green">
+        Create New Post
+      </button>
+    </form>
   );
 }
